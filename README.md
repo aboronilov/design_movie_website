bc661c0dff4c981ebc47acf9f5d49bdae943aa21
= <=>
2022-04-29 15:47:03 +0200

    slider is responsive, readme added

---
+++ README.md
@@ -0,0 +1,9 @@
+My first expirience in web design :)
+This landing page is created on pure HTML and CSS
+
+Clone repo:
+```
+    git clone https://github.com/aboronilov/design_movie_website
+```
+
+Open ##index.html file in any browser
--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow, index) => {
-   const itemNumber = movieLists[index].querySelectorAll('img').length;
-   let clickCounter = 0;
-   arrow.addEventListener('click', () => {
-      clickCounter ++;
-      if (itemNumber - (4 + clickCounter) >= 0) {
-         movieLists[index].style.transform = `translateX(${
-            movieLists[index].computedStyleMap().get("transform")[0].x.value
-         -300}px)`;
-      } else {
-         movieLists[index].style.transform = "translateX(0)";
-         clickCounter = 0;
-      }
-   });
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= -1) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+
+  console.log(Math.floor(window.innerWidth / 270));
 });
 
 const ball = document.querySelector(".toggle-ball")
